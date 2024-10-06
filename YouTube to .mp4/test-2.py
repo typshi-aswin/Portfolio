91162549f140670e6020eb3ba9c09c30898728f0
Aswin <aswinvsivan123@gmail.com>
2024-10-06 20:02:02 +0200

    Delete YouTube to .mp4/test-2.py

--- YouTube to .mp4/test-2.py
+++
@@ -1,9 +0,0 @@
-from pytubefix import YouTube
-from pytubefix.cli import on_progress
-
-url = "https://www.youtube.com/watch?v=5hPtU8Jbpg0"
-yt = YouTube(url)
-print(yt.title)
-
-stream = yt.streams.get_highest_resolution()
-stream.download()
